fed90e7801e71e86cd47f2abc98082f9601e6ab5
hyunyoung2 <hyun02.engineer@gmail.com>
2018-02-27 14:32:33 +0900

    perfect version of shell script

--- Cornell/svm_light/svm_light_with_sen2vec.sh
+++ Cornell/svm_light/svm_light_with_sen2vec.sh
@@ -4,11 +4,11 @@
 
 echo "starting skip-gram based doc2vec to learn"
 
-#./svm_learn ../../data/SVM_Light/train/SKIP_GRAM_SVM_Light_train  ./SKIP_GRAM_SVM_Light_train_model
+./svm_learn ../../data/SVM_Light/train/SKIP_GRAM_SVM_Light_train  ./SKIP_GRAM_SVM_Light_train_model
 
 echo "starting cbow based doc2vec to learn"
 
-#./svm_learn ../../data/SVM_Light/train/CBOW_SVM_Light_train ./CBOW_SVM_Light_train_model
+./svm_learn ../../data/SVM_Light/train/CBOW_SVM_Light_train ./CBOW_SVM_Light_train_model
 
 echo "staring classification sentences based on skip gram"
 echo "check the result on SKIP_GRAM_SVM_Light_test_resulr in log dir "
@@ -17,4 +17,4 @@ echo "check the result on SKIP_GRAM_SVM_Light_test_resulr in log dir "
 echo "starting classifciation senetences based on CBOW"
 echo "check the result on CBOW_SVM_Light_test_result"
 
-#./svm_classify  ../../data/SVM_Light/test/CBOW_SVM_Light_test  ./CBOW_SVM_Light_train_model ./CBOW_SVM_Light_Predictioni > CBOW_SVM_Light_test_result
+./svm_classify  ../../data/SVM_Light/test/CBOW_SVM_Light_test  ./CBOW_SVM_Light_train_model ./CBOW_SVM_Light_Predictioni > CBOW_SVM_Light_test_result
