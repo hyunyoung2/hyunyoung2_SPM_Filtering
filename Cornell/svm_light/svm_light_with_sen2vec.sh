10af07d5e0347fbb23c66cab04d017d620a4cd1c
hyunyoung2 <hyun02.engineer@gmail.com>
2018-03-07 13:00:09 +0900

    how to execute for svm light

--- Cornell/svm_light/svm_light_with_sen2vec.sh
+++ Cornell/svm_light/svm_light_with_sen2vec.sh
@@ -12,9 +12,9 @@ echo "starting cbow based doc2vec to learn"
 
 echo "staring classification sentences based on skip gram"
 echo "check the result on SKIP_GRAM_SVM_Light_test_resulr in log dir "
-./svm_classify  ../../data/SVM_Light/test/SKIP_GRAM_SVM_Light_test ./SKIP_GRAM_SVM_Light_train_model ./SKIP_GRAM_SVM_Lihgt_Prediction > SKIP_GRAM_SVM_Light_test_result
+./svm_classify  ../../data/SVM_Light/test/SKIP_GRAM_SVM_Light_test ./SKIP_GRAM_SVM_Light_train_model ./SKIP_GRAM_SVM_Lihgt_Prediction > ./log/SKIP_GRAM_SVM_Light_test_result
 
 echo "starting classifciation senetences based on CBOW"
 echo "check the result on CBOW_SVM_Light_test_result"
 
-./svm_classify  ../../data/SVM_Light/test/CBOW_SVM_Light_test  ./CBOW_SVM_Light_train_model ./CBOW_SVM_Light_Predictioni > CBOW_SVM_Light_test_result
+./svm_classify  ../../data/SVM_Light/test/CBOW_SVM_Light_test  ./CBOW_SVM_Light_train_model ./CBOW_SVM_Light_Predictioni > ./log/CBOW_SVM_Light_test_result
